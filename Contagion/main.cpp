92594ef922793ef22f3d4857e4191d2f2c8f1451
Nathan Adams <NathanAdams.@my.unt.edu>
2023-03-02 14:06:05 -0600

    Add project files.

---
+++ Contagion/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	
+	cout << "Infection Time" << endl;
+	
+	return 0;
+}
